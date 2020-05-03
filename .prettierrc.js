129720de9bc753a87fcd29377addc38440c9fcd2
phugh <peter@phugh.es>
2020-05-04 00:02:42 +0100

    add prettier config file and update .eslintrc

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  printWidth: 120,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false,
+}
