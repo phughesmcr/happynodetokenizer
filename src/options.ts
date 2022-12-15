dde0f1e8fd4b10d4201bc9ea0424fe64c2e2dc83
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2022-12-15 18:05:54 +0000

    Make option normalization more typesafe

--- src/options.ts
+++ src/options.ts
@@ -6,17 +6,21 @@ import type { TokenizerOptions, TokenizerMode, TokenizerNormalization } from "./
 export function normalizeOpts(opts: TokenizerOptions): Required<TokenizerOptions> {
   const { mode = "stanford", normalize = null, preserveCase = true } = opts;
 
+  if (typeof mode !== "string") {
+    throw new TypeError(`"mode" must be a string. Found "${typeof mode}".`);
+  }
   const _mode = mode.toLowerCase();
+  if (normalize && typeof normalize !== "string") {
+    throw new TypeError(`"normalize" must be a string. Found "${typeof normalize}".`);
+  }
   const _normalize = normalize?.toUpperCase() ?? null;
-
-  if (!MODE_PATTERN.exec(_mode)) throw new SyntaxError(`"mode" must be "stanford" or "dlatk". Found "${mode}".`);
   if (normalize && !NORM_PATTERN.exec(normalize)) {
-    throw new SyntaxError(`"normalize" option must be 'NFC' | 'NFD' | 'NFKC' | 'NFKD'. Found "${normalize}".`);
+    throw new SyntaxError(`"normalize" option must be 'NFC' | 'NFD' | 'NFKC' | 'NFKD' | null. Found "${normalize}".`);
   }
 
   return {
-    mode: typeof mode !== "string" ? "stanford" : (_mode as TokenizerMode),
-    normalize: typeof normalize !== "string" ? null : (_normalize as TokenizerNormalization),
+    mode: _mode as TokenizerMode,
+    normalize: _normalize as TokenizerNormalization,
     preserveCase: !!preserveCase,
   };
 }
